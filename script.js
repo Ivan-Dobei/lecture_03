1d0c75815f40af2c1d6a5cd3f223bd7ed7a003d1
Ivan Dobei <vanjadobej@gmail.com>
2024-10-14 13:15:06 +0300

    add pagination

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const characterList = document.querySelector(".character_list");
+const loadingTitle = document.querySelector('.loading_text');
 const previousBtn = document.querySelector(".previous_button");
 const nextBtn = document.querySelector(".next_button");
 const pagesCount = document.querySelector(".count_of_pages");
 const url = 'https://rickandmortyapi.com/api/character';
+let nextPageUrl;
+let previousPageUrl;
 
-async function fetchCharacters(url) {
-    try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.error('Fetch error:', error);
-    }
+function getPageNumberFromUrl(url) {
+    let position = url.indexOf('=');
+    return parseInt(url.slice(position+1));
 }
 
-// todo: add page counter
-// todo: add pagination
-
-fetchCharacters(url).then(response => {
+function showCharacterList(data) {
     let characterItems = '';
-    response.results.forEach(character => {
+    data.results.forEach(character => {
         characterItems += `
         <li class="character_item">
             <img class="item_img" src=${character.image}>
@@ -36,6 +24,67 @@ fetchCharacters(url).then(response => {
     `
     })
     characterList.innerHTML = characterItems;
-})
+}
+
+function pagination (data) {
+    const pageNumber = data.info.next ? getPageNumberFromUrl(data.info.next) - 1 : data.info.pages;
+    pagesCount.innerHTML = `Page: ${pageNumber}`;
+
+    if (data.info.next) {
+        nextBtn.disabled = false;
+        nextPageUrl = data.info.next;
+        nextBtn.classList.remove("disabled_btn");
+    } else {
+        nextBtn.disabled = true;
+        nextBtn.classList.add("disabled_btn");
+    }
+
+    if (data.info.prev) {
+        previousBtn.disabled = false;
+        previousPageUrl = data.info.prev;
+        previousBtn.classList.remove("disabled_btn");
+    } else {
+        previousBtn.disabled = true;
+        previousBtn.classList.add("disabled_btn");
+    }
+}
+
+async function fetchCharacters(url) {
+    characterList.classList.add("hide");
+    loadingTitle.classList.remove("hide");
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        showCharacterList(data);
+        pagination(data);
+
+    } catch (error) {
+        console.error('Fetch error:', error);
+    } finally {
+        loadingTitle.classList.add("hide");
+        characterList.classList.remove("hide");
+    }
+}
+
+fetchCharacters(url)
+
+nextBtn.addEventListener('click', () => {
+    if (nextPageUrl) {
+        fetchCharacters(nextPageUrl);
+    }
+});
+
+previousBtn.addEventListener('click', () => {
+    if (previousPageUrl) {
+        fetchCharacters(previousPageUrl);
+    }
+});
+
 
 
